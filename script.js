4791d337048d814e762599707af689042f81d2c9
designerlouise <designlouisegrafico@gmail.com>
2025-05-30 13:23:17 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Exemplo de interação para botão de "Assistir Agora"
+document.querySelector('.hero button').addEventListener('click', function() {
+  alert('Você clicou em Assistir Agora! Personalize esta ação.');
+});
